60232a3ad6e1aaed15e80504ec6cfc4e19524a77
Andre Entrekin <andre.entrekin@gmail.com>
2020-01-22 09:47:11 -0600

    put server.js in main folder

--- Develop/server.js
+++ Develop/server.js
@@ -4,16 +4,17 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../../../public/index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
     // res.send("Hello World")
 });
 app.get("/notes", function (req, res) {
-    res.json(path.join(__dirname, "public/notes.html"));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 
