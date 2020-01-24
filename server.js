0522b158a6fb85eb9d9b4bf49816b245eceff3e9
Andre Entrekin <andre.entrekin@gmail.com>
2020-01-24 10:02:05 -0600

    added stringify

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 var express = require("express")
 var path = require("path");
 const fs = require('fs');
-var index = ("./public/assets/js/index.js")
+// var index = require("./public/assets/js/index")
 var app = express();
+var db = require("./db/db.json");
 var PORT = process.env.PORT || 3000;
 // need to read the json file and parse it to properly import json
-const db = fs.readFile('./db/db.json', function (err, data) {
+// const db = fs.readFile('./db/db.json', function (err, data) {
+//     console.log(data + "is here");
 
-})
-console.log("this is db" + db)
-
+// })
+// console.log("this is db" + db)
+var dataToGo = []
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -28,18 +30,29 @@ app.get("/notes", function (req, res) {
 
 //API routes
 app.get("/api/notes", function (req, res) {
-    res.json(db)
+    fs.readFile('./db/db.json', function (err, data) {
+        console.log(data + "is here");
+        res.json(data)
+
+    })
 });
 app.post('/api/notes', function (req, res) {
-    db.push(req.body); // add note to the json
-    fs.writeFile('./db/db.json', req.body, function () {
-        //write to json
-    })
-    res.json(db);
+    console.log(req.body + "is here");
+    db.push(req.body);
+    console.log(db);
+    let data = JSON.stringify(db, null, 2);
 
-})
+    fs.writeFile("./db/db.json", data, function (err) {
+        if (err) throw err
+        console.log("write is working");
+
+    })
+    res.json("data saved")
+
+});
+
 
 
 app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+    console.log('Server listening on: http://localhost:' + PORT);
 });
